b6db6d038e75a5d0143b5eb3a548e5e2247cf088
ajithbabu <ajithbabu@personaluse.com>
2025-04-07 21:44:30 +0530

    category ui change

---
+++ lib/ui_update.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyUi_Changes extends StatefulWidget {
+  const MyUi_Changes({super.key});
+
+  @override
+  State<MyUi_Changes> createState() => _MyUi_ChangesState();
+}
+
+class _MyUi_ChangesState extends State<MyUi_Changes> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
